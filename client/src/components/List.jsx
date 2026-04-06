4a46c8ffa209327e6e77b1a241a87bd81eec9b23
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-04-06 14:06:01 -0400

    Add Chat model to schema.prisma

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -4,7 +4,7 @@ import Card from "../components/Card";
 function List({ posts }) {
   return (
     <div className="list ">
-      {posts.map((item) => (
+      {posts?.map((item) => (
         <Card key={item.id} item={item} />
       ))}
     </div>
